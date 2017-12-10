ea239aeb33f5515dbc075134b50cbb74a22cde6d
saad <saadodi44@gmail.com>
2017-12-10 14:03:54 +0200

    add ability to select directories + add recursion checking

--- src/main/java/me/demoniaque/htac/Controller.java
+++ src/main/java/me/demoniaque/htac/Controller.java
@@ -3,33 +3,59 @@ package me.demoniaque.htac;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
 
 public class Controller {
 
 	@FXML
 	public Button selectFiles;
 
-	public void run(ActionEvent actionEvent) {
+	public void fileChooserAction(ActionEvent actionEvent) {
 		FileChooser fileChooser = new FileChooser();
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("text or json", "*.txt", "*.json");
-		fileChooser.getExtensionFilters().add(extFilter);
+		fileChooser.getExtensionFilters().addAll(
+				new FileChooser.ExtensionFilter("txt", "*.txt"),
+				new FileChooser.ExtensionFilter("json", "*.json")
+		);
 		List<File> files = fileChooser.showOpenMultipleDialog(Main.primaryStage);
 
-		if (files.isEmpty()) return;
+		if (files == null || files.isEmpty()) return;
 
+		convert(files);
+	}
+
+	public void directoryChooserAction(ActionEvent actionEvent) {
+		DirectoryChooser chooser = new DirectoryChooser();
+		File dir = chooser.showDialog(Main.primaryStage);
+
+		if (dir == null) return;
+
+		HashSet<File> files = new HashSet<>();
+		try {
+			Files.find(Paths.get(dir.getPath()), 999, (p, bfa) ->
+					bfa.isRegularFile()
+							&& (p.getFileName().toString().matches(".*\\.json")
+							|| p.getFileName().toString().matches(".*\\.txt")))
+					.forEach(path -> files.add(path.toFile()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		convert(files);
+	}
+
+	public void convert(Collection<File> files) {
 		for (File file : files) {
-			if (file == null) continue;
-
 			try {
 				List<String> fileContent = new ArrayList<>(Files.readAllLines(Paths.get(file.getPath()), StandardCharsets.UTF_8));
 
